f87a9d38fab44348c6148199fb7bcb9e186f6faf
Amon-Harsh <amonharsh11@gmail.com>
2025-05-26 14:27:53 +0530

    complete

--- Final_proj/app/routes/tickets.py
+++ Final_proj/app/routes/tickets.py
@@ -1,4 +1,4 @@
-#It handles ticket related routes
+#It handles ticket related routes(complete)
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from app.models import Ticket, TicketMessage, User
